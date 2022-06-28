ad9f73764d3ac6a31c31e7f2253c9d22b060ad50
Diya S <diya.diyu14@gmail.com>
2022-06-29 00:53:26 +0530

    added test suite for availability testing

--- availability.test.js
+++ availability.test.js
@@ -1,6 +1,43 @@
-const productDetails = require('./productDetails');
+const productDetails = require("./productDetails");
 
-test('Show correct availability', async () => {
-    const res = await productDetails.getAvailability('B01F1CE2NM', productDetails.fetchProduct);
-    expect(res).toBe('Only 1 left in stock.');
-})
+describe("Product availability tests", () => {
+  test("Checking availability for a product in stock", async () => {
+    const res = await productDetails.getAvailability(
+      "B08T947SVY",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("In stock.");
+  });
+
+  test("Checking availability for a product having 1 item in stock", async () => {
+    const res = await productDetails.getAvailability(
+      "B01F1CE2NM",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("Only 1 left in stock.");
+  });
+
+  test("Checking availability for a product that's out of stock", async () => {
+    const res = await productDetails.getAvailability(
+      "B01MXTQCD7",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("Currently unavailable.");
+  });
+
+  test("Checking availability for a product with no vendors", async () => {
+    const res = await productDetails.getAvailability(
+      "B08FWG4376",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("No vendor available for product sale.");
+  });
+
+  test("Checking availability for an invalid ASIN", async () => {
+    const res = await productDetails.getAvailability(
+      "INVALIDASI",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("AxiosError");
+  });
+});
--- productDetails.js
+++ productDetails.js
@@ -1,82 +1,109 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-
 const fetchProduct = async (productASIN) => {
-    try {
-        const response = await axios.get(`https://www.amazon.in/dp/${productASIN}`);
- 
-        const html = response.data;
- 
-        const $ = cheerio.load(html);
+  try {
+    const response = await axios.get(`https://www.amazon.in/dp/${productASIN}`);
 
-        const title = $('#productTitle').text().trim();
+    const html = response.data;
 
-        const image = $('#landingImage').attr();
-        const imageLink = image.src;
+    const $ = cheerio.load(html);
 
-        const availability = $('#availability').children().first().text().trim();
+    const title = $("#productTitle").text().trim();
 
-        const noReviews = $('#acrCustomerReviewText').text();
-        const reviewCount = noReviews.replace(/[^0-9]/g,'').trim();
+    const image = $("#landingImage").attr();
+    const imageLink = image.src;
 
-        const ratingStars = $('#averageCustomerReviews .a-icon.a-icon-star .a-icon-alt').text().trim();
-        const rating = ratingStars.split(' ')[0];
-    
-        // prices = []
-        // prices.push($('#corePrice_feature_div .a-price-whole').text());
-        // prices.push($('#corePrice_feature_div .a-offscreen').text());
+    let availability = $("#availability").children().first().text().trim();
+    availability =
+      availability === ""
+        ? "No vendor available for product sale."
+        : availability;
 
-        features_list = [];
-        $('#feature-bullets ul').children().each((i, feature) => {
-            features_list.push($(feature).text().trim());
-        })
+    const noReviews = $("#acrCustomerReviewText").text();
+    const reviewCount = noReviews.replace(/[^0-9]/g, "").trim();
 
-        let product = {
-            title,
-            link: `https://www.amazon.in/dp/${productASIN}`,
-            imageLink,
-            availability,
-            reviewCount,
-            rating,
-            features_list
-        }
+    const ratingStars = $(
+      "#averageCustomerReviews .a-icon.a-icon-star .a-icon-alt"
+    )
+      .text()
+      .trim();
+    const rating = ratingStars.split(" ")[0].trim();
 
-        return product;
-        
-    } catch (err) {
-        console.log(err);
-    }
-}
+    // prices = []
+    // prices.push($('#corePrice_feature_div .a-price-whole').text());
+    // prices.push($('#corePrice_feature_div .a-offscreen').text());
+
+    features_list = [];
+    $("#feature-bullets ul")
+      .children()
+      .each((i, feature) => {
+        features_list.push($(feature).text().trim());
+      });
+
+    let product = {
+      title,
+      link: `https://www.amazon.in/dp/${productASIN}`,
+      imageLink,
+      availability,
+      reviewCount,
+      rating,
+      features_list,
+    };
+
+    return product;
+  } catch (err) {
+    return err.name;
+  }
+};
 
 const getTitle = (productASIN, fetchProduct) => {
-    return fetchProduct(productASIN).then(res => res.title);
-}
+  return fetchProduct(productASIN).then((res) => {
+    return typeof res === "object" ? res.title : res;
+  });
+};
 
 const getImage = (productASIN, fetchProduct) => {
-    return fetchProduct(productASIN).then(res => res.imageLink);
-}
+  return fetchProduct(productASIN).then((res) => {
+    return typeof res === "object" ? res.imageLink : res;
+  });
+};
 
 const getAvailability = (productASIN, fetchProduct) => {
-    return fetchProduct(productASIN).then(res => res.availability);
-}
+  return fetchProduct(productASIN).then((res) => {
+    return typeof res === "object" ? res.availability : res;
+  });
+};
 
 const getReviewCount = (productASIN, fetchProduct) => {
-    return fetchProduct(productASIN).then(res => res.reviewCount);
-}
+  return fetchProduct(productASIN).then((res) => {
+    return typeof res === "object" ? res.reviewCount : res;
+  });
+};
 
 const getRating = (productASIN, fetchProduct) => {
-    return fetchProduct(productASIN).then(res => res.rating);
-}
+  return fetchProduct(productASIN).then((res) => {
+    return typeof res === "object" ? res.rating : res;
+  });
+};
 
 const getFeatures = (productASIN, fetchProduct) => {
-    return fetchProduct(productASIN).then(res => res.features_list);
-}
+  return fetchProduct(productASIN).then((res) => {
+    return typeof res === "object" ? res.features_list : res;
+  });
+};
 
 // prodList = ['B0987Y1MBZ', 'B01F1CE2NM', 'B08GYMZP5L', 'B07XJPLL27', 'B08DKX84WG', 'B08MC5GLG4'];
-// prodList.forEach((asin, id) => {
-//     fetchProduct(asin, id).then(res => console.log(res));
+// prodList.forEach((asin) => {
+//     fetchProduct(asin).then(res => console.log(res));
 // });
 
-
-module.exports = {fetchProduct, getTitle, getImage, getAvailability, getReviewCount, getRating, getFeatures};
+module.exports = {
+  fetchProduct,
+  getTitle,
+  getImage,
+  getAvailability,
+  getReviewCount,
+  getRating,
+  getFeatures,
+};
