49d9efaed1970e32e4bc5ee43b38b0fc47b6e305
Diya S <diya.diyu14@gmail.com>
2022-07-02 12:57:05 +0530

    added testcases to title.test.js

--- title.test.js
+++ title.test.js
@@ -1,13 +1,30 @@
 const productDetails = require("./productDetails");
 
 // Testing for title of product
+describe("Product title tests", () => {
+  test("Show product title for a valid ASIN", async () => {
+    const res = await productDetails.getTitle(
+      "B08VN53D3P",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("USPA Men T-Shirt");
+  });
 
-test("Show correct product title", async () => {
-  const res = await productDetails.getTitle(
-    "B08GYMZP5L",
-    productDetails.fetchProduct
-  );
-  expect(res).toBe(
-    "Bryan & Candy New York Body Fragrance Mist Spray FAB FIVE Combo Gift Set For Women, 115 ml Each (Pack of 5) No Gas Perfume"
-  );
+  test("Show correct product title", async () => {
+    const res = await productDetails.getTitle(
+      "B08GYMZP5L",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe(
+      "Bryan & Candy New York Body Fragrance Mist Spray FAB FIVE Combo Gift Set For Women, 115 ml Each (Pack of 5) No Gas Perfume"
+    );
+  });
+
+  test("Show product title for an invalid ASIN", async () => {
+    const res = await productDetails.getTitle(
+      "INVALIDASI",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("AxiosError");
+  });
 });
