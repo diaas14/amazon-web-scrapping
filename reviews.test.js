a97af7fc68392ab827ccac234b59d392b0c5dee5
Chaitali shekar <chai@Chaitalis-MacBook-Pro.local>
2022-07-02 13:25:03 +0530

    added 3 testcases for reviews

---
+++ reviews.test.js
@@ -0,0 +1,31 @@
+const productDetails = require("./productDetails");
+
+  test("Show correct number of reviews", async () => {
+    const res = await productDetails.getReviewCount(
+      "B01F1CE2NM",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("488");
+
+});
+   
+test("Show that product has no reviews", async () => {
+    const res = await productDetails.getReviewCount(
+      "B09ZLQXMXX",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("");
+    
+
+
+});
+
+test("Checking reviews for invalid product", async () => {
+    const res = await productDetails.getReviewCount(
+      "INVALIDASI",
+      productDetails.fetchProduct
+    );
+    expect(res).toBe("AxiosError");
+
+});
+
